3aaa1aa2fbb5980663a3b7b251c15ca25c923ec6
dev <devendrajenatrichy@gmail.com>
2024-10-24 21:46:34 +0530

    first commit

---
+++ src/main/java/com/example/maxxengg/MaxxenggApplication.java
@@ -0,0 +1,13 @@
+package com.example.maxxengg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxxenggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxxenggApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maxxengg/MaxxenggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maxxengg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxxenggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
